1da0b7feb24001b062af3a89643af651abaaa339
Anon873-cyber <async2304@gmail.com>
2026-02-19 16:31:01 +0530

    s

--- src/components/index.js
+++ src/components/index.js
@@ -3,8 +3,10 @@ import Footer from "./Footer/Footer";
 import Container from '../components/container/Container'
 import Logo from '../components/Logo'
 import LogoutBtn from '../components/Header/LogoutBtn'
+import RTE from "./RTE";
 
 export {
+  RTE,
   Header,
   Footer,
   Container,
