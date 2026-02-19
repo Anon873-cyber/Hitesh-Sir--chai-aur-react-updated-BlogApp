5ff2b92cf0a7b298a03ec4dbddc08ceb4beca6bc
Anon873-cyber <async2304@gmail.com>
2026-02-20 01:20:56 +0530

    created home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+import appwriteService from '../appwrite/config'
+import { Container,PostCard } from '../components'
+
+const Home = () => {
+    const 
+  return 
+  (
+    <div>Home</div>
+  )
+}
+
+
+export default Home
