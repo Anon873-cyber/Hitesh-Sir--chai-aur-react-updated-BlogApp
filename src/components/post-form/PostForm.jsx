b9524f8a9e1e6318a19a47c473fe7b5a1442e3b3
Anon873-cyber <async2304@gmail.com>
2026-02-19 16:27:53 +0530

    Added RTE in index.js

---
+++ src/components/post-form/PostForm.jsx
@@ -0,0 +1,10 @@
+import React,{useCallback} from 'react'
+import { useForm } from 'react-hook-form'
+import {Button ,Input,Select}
+ const PostForm = () => {
+  return (
+    <div>PostForm</div>
+  )
+}
+
+export default PostForm
